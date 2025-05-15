7c28af1a542487610e64060670e515ef7846ccbd
Shoji <yulinzone2008@yahoo.co.jp>
2025-05-15 16:46:32 +0900

    カテゴリ追加機能を実装

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,9 @@ import { db } from "@/db";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { DashboardPageContent } from "./dashboard-page-content";
+import { CreateEventCategoryModal } from "@/components/create-event-category-modal";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
 
 const Page = async () => {
   const auth = await currentUser();
@@ -18,9 +21,21 @@ const Page = async () => {
     redirect("/sign-in");
   }
 
-  return <DashboardPage title="Dashboard">
-    <DashboardPageContent />
-  </DashboardPage>;
+  return (
+    <DashboardPage
+      cta={
+        <CreateEventCategoryModal>
+          <Button>
+            <PlusIcon className="size-4 mr-2"/>
+            Add Category
+          </Button>
+        </CreateEventCategoryModal>
+      }
+      title="Dashboard"
+    >
+      <DashboardPageContent />
+    </DashboardPage>
+  );
 };
 
 export default Page;
